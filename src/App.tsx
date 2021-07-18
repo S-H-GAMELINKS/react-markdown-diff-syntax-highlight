1701a5cfcaca0cde4922be03e8efc01d3da9df07
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-07-18 22:16:25 +0900

    render markdwon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import logo from './logo.svg';
 import './App.css';
+
+const content = ' ```ruby\nhoge\nfuga\n```\n';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMarkdown children={
+        content
+      } />
     </div>
   );
 }
